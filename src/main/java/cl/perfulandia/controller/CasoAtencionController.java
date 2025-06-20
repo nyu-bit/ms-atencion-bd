a00d59edaac53c83d4932b34d7a88ffcee73c55b
María José Contreras González <mj.contrerasg@duocuc.cl>
2025-06-20 19:44:06 -0400

    Update CasoAtencionController.java

--- src/main/java/cl/perfulandia/controller/CasoAtencionController.java
+++ src/main/java/cl/perfulandia/controller/CasoAtencionController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import cl.perfulandia.model.dto.CasoAtencionDTO;
 import cl.perfulandia.service.CasoAtencionService;
